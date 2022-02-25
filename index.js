3ea83e39571ce1ae58180624f80b96ba5f4a93e8
Fabio Teixeira da Costa <ftcosta@uol.com.br>
2022-02-25 13:00:38 -0300

    Update index.js
    Nova rota adicionada

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req,res) => {
     res.status(200).send({message: 'Olá mundo estou esse é meu novo repositório'});
 })
 
+app.get('/ola', (req,res) => {
+    res.status(200).send({message: 'Olá mundo estou testando desktop'});
+})
+
+
 app.listen(4001, () => {
     console.log('Api rodando na porta 4001');
 })
